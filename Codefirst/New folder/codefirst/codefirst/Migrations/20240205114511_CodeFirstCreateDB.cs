e40bb3a1c68b6a752d11b12b698c9dab358a34ec
abhishekb13402 <abhishekbhatt13402@gmail.com>
2024-02-05 18:56:01 +0530

    Implement Code First Approach

---
+++ Codefirst/New folder/codefirst/codefirst/Migrations/20240205114511_CodeFirstCreateDB.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace codefirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class CodeFirstCreateDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentTable",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Student_Name = table.Column<string>(type: "varchar(10)", nullable: false),
+                    Student_Gender = table.Column<string>(type: "varchar(10)", nullable: false),
+                    Student_Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentTable", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentTable");
+        }
+    }
+}
---
+++ Codefirst/New folder/codefirst/codefirst/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace codefirst.Models
+{
+    [Table("StudentTable")]
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column("Student_Name", TypeName ="varchar(10)")]
+        public string Name { get; set; }
+        [Required]
+        [Column("Student_Gender", TypeName = "varchar(10)")]
+        public string Gender { get; set; }
+        [Column("Student_Age")]
+        public int Age { get; set; }
+    }
+}
