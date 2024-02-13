f9c1c9bd346cd80d9232c5b6b121aa8c68a99d97
abhishekb13402 <abhishekbhatt13402@gmail.com>
2024-02-14 00:55:42 +0530

    Asp core api project implement httpget, httpget with id, httppost

---
+++ ASPCoreWebAPI project/ASPCoreWebAPIEFCoreDB/ASPCoreWebAPIEFCoreDB/Controllers/StudentAPIController.cs
@@ -0,0 +1,62 @@
+﻿using ASPCoreWebAPIEFCoreDB.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPCoreWebAPIEFCoreDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        private readonly AbhidbContext context;
+
+        public StudentAPIController(AbhidbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudents()
+        {
+            var data = await context.Students.ToListAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudentById(int id)
+        {
+            var student = await context.Students.FindAsync(id);
+            if(student == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return student;
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Student>> CreateStudent(Student std)
+        {
+            await context.Students.AddAsync(std);
+            await context.SaveChangesAsync();
+            return Ok(std);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Student>> UpdateStudent(int id, Student std)
+        {
+            if(id != std.Id)
+            {
+                return BadRequest();
+            }
+            context.Entry(std).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(std);
+        }
+
+
+    }
+}
---
+++ ASPCoreWebAPI project/ASPCoreWebAPIEFCoreDB/ASPCoreWebAPIEFCoreDB/Models/AbhidbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPCoreWebAPIEFCoreDB.Models;
+
+public partial class AbhidbContext : DbContext
+{
+    public AbhidbContext()
+    {
+    }
+
+    public AbhidbContext(DbContextOptions<AbhidbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Gender).HasMaxLength(50);
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
