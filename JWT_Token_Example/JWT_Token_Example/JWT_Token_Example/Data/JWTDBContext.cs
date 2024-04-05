fc07e7b2f7633d33a2de7c866179e19db24f6376
tntra <tntra@DESKTOP-F6047KK>
2024-04-05 13:07:19 +0530

    jwt test, js edit password gen file

---
+++ JWT_Token_Example/JWT_Token_Example/JWT_Token_Example/Data/JWTDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JWT_Token_Example.Data
+{
+    public class JWTDBContext : DbContext
+    {
+        public JWTDBContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+    }
+}
