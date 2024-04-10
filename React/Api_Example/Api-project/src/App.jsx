bbb1aed0aa617dcf8cab1d997ed6f18deb9c9429
abhishekb13402 <abhishekbhatt13402@gmail.com>
2024-04-10 21:42:34 +0530

    react api example

---
+++ React/Api_Example/Api-project/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const jsonData = await response.json();
+      setData(jsonData);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>List of Users</h1>
+      <ul>
+        {data.map(user => (
+          <li key={user.id}>
+            {user.name} ({user.email})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
